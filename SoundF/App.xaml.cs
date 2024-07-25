93f0a15f698ce75c5bce007cca2b0308caf87145
LuoLuoHu <1987699744@qq.com>
2024-07-25 18:57:20 +0800

    First

---
+++ SoundF/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SoundF
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new AppShell();
+		}
+	}
+}
---
+++ SoundF/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SoundF
+{
+	public partial class AppShell : Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+		}
+	}
+}
